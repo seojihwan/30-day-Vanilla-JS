b2ff0952214900d35f62ff7bc56a3d4d699e4e21
seojihwan <seojihwan213@naver.com>
2020-07-22 17:56:35 +0900

    1 day drumkit

---
+++ 1day/index.js
@@ -0,0 +1,57 @@
+// array for keyCode
+const keys = [65, 83, 68, 70, 71, 72, 74, 75, 76];
+// array for soundFile
+const keyNames = [
+  'boom',
+  'clap',
+  'hihat',
+  'kick',
+  'openhat',
+  'ride',
+  'snare',
+  'tink',
+  'tom',
+];
+// wrapper Div
+const keysWrapper = document.createElement('div');
+keysWrapper.className = 'keysWrapper';
+document.body.appendChild(keysWrapper);
+
+// create keyboard and audio element.
+keys.forEach((key, idx) => {
+  const keyboard = document.createElement('div');
+  const audio = document.createElement('audio');
+  keyboard.className = 'key';
+  keyboard.innerHTML = keyNames[idx];
+  keyboard.setAttribute('keyCode', key);
+  audio.src = `./sound/${keyNames[idx]}.wav`;
+  audio.setAttribute('keyCode', key);
+  document.body.appendChild(audio);
+  keysWrapper.appendChild(keyboard);
+});
+
+// if transition ended classList('playing') is removed.
+const removeTransition = (e) => {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+};
+
+// each keyboards has an transitionend EventListner.
+const keyboards = document.querySelectorAll('.key');
+keyboards.forEach((keyboard) => {
+  keyboard.addEventListener('transitionend', removeTransition);
+});
+
+// add classList to downKey and play audio using event.keyCode.
+const playAudio = (e) => {
+  const downKey = document.querySelector(`.key[keyCode="${e.keyCode}"]`);
+  if (!downKey) return;
+  downKey.classList.add('playing');
+  const audio = document.querySelector(`audio[keyCode="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+};
+
+// window has a keydown EventListner
+window.addEventListener('keydown', playAudio);
