c3df9111deebe4bc00f0d3e0cc5932bced4fbe95
seo ji-hwan <seojihwan213@naver.com>
2020-08-05 18:21:23 +0900

    fix code

--- 10day/index.js
+++ 10day/index.js
@@ -3,10 +3,9 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 let lastChecked;
 
 function handleCheck(e) {
-  let inBetween = false;
+  let inBetween = false; //콜백함수 내에서 각 checkbox의 상태를 확인
   if (e.shiftKey && this.checked) {
     checkboxes.forEach((checkbox) => {
-      console.log(checkbox);
       if (checkbox === this || checkbox === lastChecked) {
         inBetween = !inBetween;
       }
@@ -16,8 +15,7 @@ function handleCheck(e) {
       }
     });
   }
-
-  lastChecked = this;
+  lastChecked = this.checked ? this : undefined;
 }
 
 checkboxes.forEach((checkbox) => checkbox.addEventListener('click', handleCheck));
