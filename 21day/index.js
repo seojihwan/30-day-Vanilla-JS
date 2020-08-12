a6b517f664ec7b0786d985219d587d08a4d3c16f
seo ji-hwan <seojihwan213@naver.com>
2020-08-12 22:52:22 +0900

    day 21 Geolocation based Speedometer and Compass

---
+++ 21day/index.js
@@ -0,0 +1,13 @@
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed');
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate ${data.coords.heading}deg`;
+  },
+  (error) => {
+    console.error(error);
+  }
+);
