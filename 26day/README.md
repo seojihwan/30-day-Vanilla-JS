f4c2afeea8e220234df7b732211ba19b6c08ac4e
seojihwan <seojihwan213@naver.com>
2020-08-17 22:13:13 +0900

    26 day Stripe Follow Along Dropdown

---
+++ 26day/README.md
@@ -0,0 +1,5 @@
+### 26 day 
+
+##### 결과화면
+
+![image](./Stripe_Follow_Along_Dropdown.gif)
---
+++ 26day/index.js
@@ -0,0 +1,44 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handleEnter() {
+  console.log('enter');
+  this.classList.add('trigger-enter');
+  setTimeout(() => {
+    this.classList.add('trigger-enter-active');
+  }, 100);
+  background.classList.add('open');
+
+  const dropdown = this.querySelector('.dropdown');
+  dropdown.getBindinga;
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  console.log(dropdownCoords);
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    width: dropdownCoords.width,
+    height: dropdownCoords.height,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left,
+  };
+
+  console.log(coords);
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty(
+    'transform',
+    `translate(${coords.left}px,${coords.top}px)`
+  );
+}
+function handleLeave() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  console.log('leave');
+  background.classList.remove('open');
+}
+triggers.forEach((trigger) =>
+  trigger.addEventListener('mouseenter', handleEnter)
+);
+triggers.forEach((trigger) =>
+  trigger.addEventListener('mouseleave', handleLeave)
+);
